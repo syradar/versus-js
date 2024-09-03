1ac16e3202bbf0af5ff892cce7ed79ce684fdcdc
David Malmström <syntex@outlook.com>
2024-09-04 01:05:10 +0200

    performance marks

--- imports/main.mjs
+++ imports/main.mjs
@@ -34,8 +34,31 @@ function gatherImports() {
  */
 async function initializeModule(name, importFunction, view) {
 	try {
+		performance.mark(`module-${name}-start`);
+		performance.mark(`import-${name}-start`);
 		const module = await importFunction();
+		performance.mark(`import-${name}-end`);
+		performance.measure(
+			`import-${name}`,
+			`import-${name}-start`,
+			`import-${name}-end`,
+		);
+
+		performance.mark(`init-${name}-start`);
 		module.init(view);
+		performance.mark(`init-${name}-end`);
+		performance.measure(
+			`init-${name}`,
+			`init-${name}-start`,
+			`init-${name}-end`,
+		);
+
+		performance.mark(`module-${name}-end`);
+		performance.measure(
+			`module-${name}`,
+			`module-${name}-start`,
+			`module-${name}-end`,
+		);
 		return Promise.resolve({ name, view });
 	} catch (error) {
 		return Promise.reject({ name, view });
@@ -63,7 +86,12 @@ function logResults(results) {
 }
 
 async function main() {
-	Promise.allSettled(gatherImports()).then(logResults);
+	performance.mark("main-start");
+
+	await Promise.allSettled(gatherImports()).then(logResults);
+
+	performance.mark("main-end");
+	performance.measure("main", "main-start", "main-end");
 }
 
 main();
